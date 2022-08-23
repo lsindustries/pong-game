71d021e30dda1eb547ba9b5d0e193ef58997873f
Lukasz S <ukasz668@gmail.com>
2022-08-23 18:31:15 +0200

    ball moving and collision detect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(800, 600)
@@ -7,8 +9,10 @@ screen.bgcolor("black")
 screen.title("Pong Game 2022")
 screen.tracer(0)
 
-r_paddle = Paddle(350,0)
-l_paddle = Paddle(-350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
 
 screen.listen()
 screen.onkey(r_paddle.up, "Up")
@@ -19,9 +23,17 @@ screen.onkey(l_paddle.down, "s")
 
 game_on = True
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
+    #detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 333 or ball.distance(l_paddle) and ball.xcor() < -333:
+        print("colision")
+        ball.bounce_x()
 
 
 
--- paddle.py
+++ paddle.py
@@ -1,23 +1,22 @@
 from turtle import Turtle
 
 
-class Paddle():
+class Paddle(Turtle):
 
-    def __init__(self, x, y):
-        self.paddle = Turtle()
-        self.paddle.penup()
-        self.paddle.shape("square")
-        self.paddle.color("white")
-        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
-        self.paddle.goto(x, y)
-
+    def __init__(self, position):
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
 
 
     def up(self):
-        new_y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
 
     def down(self):
-        new_y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
